f5aee8afdbb7c0719e7edabce9fb99d62060b07e
Apple <macbook@Apples-MacBook-Pro.local>
2023-12-12 12:52:14 +0545

    changes

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 import users.views as user_views
+
 urlpatterns = [
-    path("", user_views.index, name="index"),
-    path("login", user_views.login, name="login"),
-    path("register", user_views.register, name="register")
-    
-    
+    path("",user_views.index, name="index"),
+    path("login",user_views.user_login,name="login"),
+    path("register",user_views.user_register,name="register"),
+    path("profile",user_views.user_profile,name="profile"),
+    path("logout",user_views.user_logout,name="logout"),
 ]
